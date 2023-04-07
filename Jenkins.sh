0093fcb235762f98d367e104000ff41fdda65eea
Techclomate <111147153+Techclomate@users.noreply.github.com>
2023-04-07 20:45:18 +0500

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins
-sudo apt install systemctl
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+sudo service jenkins start
+sudo service jenkins status
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
